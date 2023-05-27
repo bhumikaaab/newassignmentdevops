35c57a79b11329ebc37feb495db033ec3d1c1bc3
I527383 <bhumika.bhandari@sap.com>
2023-05-27 19:24:48 +0530

    First Commit

---
+++ src/main/java/com/demo/devops/AssingmentNewDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssingmentNewDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssingmentNewDevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/devops/AssingmentNewDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssingmentNewDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
